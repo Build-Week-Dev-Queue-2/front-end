558985ef22296dddc8f9a5ffb7650c3722f871f1
Blayze <blayzestone@protonmail.com>
2020-05-25 16:08:03 -0700

    create PrivateRoute component
    routes the user if there is a token in state otherwise redirects them
    back to the login route.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import PrivateRoute from "./components/PrivateRoute";
 import LoginForm from "./components/Forms/LoginForm";
 import RegisterForm from "./components/Forms/RegisterForm";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Router>
       <Switch>
+        <PrivateRoute path="/home" component={() => <div>Hello World</div>} />
         <Route path="/register" component={RegisterForm} />
         <Route path="/login" component={LoginForm} />
         <Route path="/" component={LoginForm} />
---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Route, Redirect } from "react-router-dom";
+
+export default function PrivateRoute({ component: Component, ...rest }) {
+  const token = useSelector((state) => state.user.token);
+
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return token ? <Component /> : <Redirect to="/login" />;
+      }}
+    />
+  );
+}
