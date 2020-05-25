ec46340f14548f937a6ad753b850ffe69f87d190
Blayze <blayzestone@protonmail.com>
2020-05-25 16:06:20 -0700

    setup redux store with login/register actions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { logger } from "redux-logger";
+import reducer from "./reducers/";
+import App from "./App";
+import "./index.css";
+
+const store = createStore(reducer, applyMiddleware(logger));
 
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <CssBaseline />
     <App />
-  </>,
+  </Provider>,
   document.getElementById("root")
 );
